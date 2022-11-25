231e998c90826634cedddb20939ea826849d0d87
gabriel <gm641490@gmail.com>
2022-11-24 21:12:37 -0300

    .

---
+++ app.js
@@ -0,0 +1,29 @@
+let fome = 5
+let sono = 10
+let banho = 'sim'
+/*
+if(fome > sono){
+    document.getElementById("mensagem").innerHTML = "Jantar"
+}
+*/
+
+/*
+if(fome > sono){
+    document.getElementById("mensagem").innerHTML = "Jantar"
+}else{
+    document.getElementById("mensagem").innerHTML = "Dormir"
+}
+Operadores relacionais
+> maior
+== igual
+< menor
+>= maior igual
+<= menor igual
+!= diferente
+
+Operadores Lógicos
+&& - E - todas as operações devem ser verdadeiras para o resultado ser verdadeiro
+
+|| - OU
+! - NÃO
+*/
